334b0afe169877f01bd1148266a255d430a18188
LeeJiyoung <99225571+jiyoung1814@users.noreply.github.com>
2023-11-06 16:54:38 +0900

    init

---
+++ app.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(express.json()); 
+
+app.listen(port,()=>{
+    console.log(`server start on port ${port}`);
+});
